9b02544db4d8f70e702d97beaaf41eb3a9e71675
danieleangeletti <angelettidaniele@gmail.com>
2023-12-15 18:02:36 +0100

    versione provvisoria completata

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,21 +1,25 @@
-const number_container = document.getElementById("number-container");
+const numberContainer = document.getElementById("number-container");
+const myArray = [];
+const userArray = [];
+const guessedNumbers = [];
 
 for (let i = 0; i < 5; i++) {
     
-    random_number = generateRandomInt(1, 100);
+    const randomNumber = generateRandomInt(1, 100);
+    myArray.push(randomNumber);
     
     if (i < 5 - 1) {
-        number_container.innerHTML += random_number + ", ";
+        numberContainer.innerHTML += randomNumber + ", ";
     }
 
     else {
-        number_container.innerHTML += random_number;
+        numberContainer.innerHTML += randomNumber;
     }
 
 }
 
-setTimeout (disappearance, 30000);
-
+setTimeout (disappearance, 3000);
+setTimeout (fiveUserNumbers, 3010);
 
 // FUNCTION
 
@@ -27,6 +31,35 @@ function generateRandomInt(min, max) {
 
 function disappearance() {
 
-    number_container.classList.add("d-none");
+    numberContainer.classList.add("d-none");
+    console.log(myArray);
+
+}
+
+function fiveUserNumbers() {
+
+    for (let i = 0; i < 5; i++) {
+        
+        const userNumber = parseInt(prompt("Inserisci un numero:"));
+        userArray.push(userNumber);
+
+    }
+
+    console.log(userArray);
+
+    let score = 0;
+
+    for (let i = 0; i < myArray.length; i++) {
+        for (let j = 0; j < userArray.length; j++) {
+
+            if (myArray[i] == userArray[j]) {
+                
+                score ++;
+
+            }            
+        }
+    }
+
+    console.log("Hai indovinato " + score + " numeri!");
 
 }
